3042c5d10d08c4a9a98b4363398c70b21ac34757
nalinbot <nalinnagar1@gmail.com>
2021-07-08 19:02:10 -0400

    Created attention.py :fire:
    Contains attention mechanism for decoder

---
+++ model/attention.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import os
+import contractions
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+import time
+import rich
+from rich.progress import track
+import spacy
+
+
+class LuongAttention(tf.keras.layers.Layer):
+    def __init__(self, params):
+        super(LuongAttention, self).__init__()
+
+        self.tdfc = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(params.gru_units))
+
+    def call(self, en_seq, dec_out):
+        scores = tf.keras.backend.batch_dot(self.tdfc(en_seq), dec_out, axes=(2, 2))
+
+        attention_weights = tf.nn.softmax(scores, axis=1)
+
+        mul = en_seq * attention_weights
+
+        context_vector = tf.reduce_mean(mul, axis=1)
+
+
+        return context_vector, attention_weights
+      
+      
